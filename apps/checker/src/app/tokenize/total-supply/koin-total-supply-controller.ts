d5c45bbb2322785c5da8ee83f40a5865bace9f7a
Frank Weijers <frank@weijers.io>
2024-05-22 23:11:55 +0200

    Include unclaimed koins in total-supply result + add json response

--- apps/checker/src/app/tokenize/total-supply/koin-total-supply-controller.ts
+++ apps/checker/src/app/tokenize/total-supply/koin-total-supply-controller.ts
@@ -1,9 +1,10 @@
 import { add, divide, multiply, round, subtract } from 'mathjs';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TotalSupplyService } from '../service/total-supply.service';
 import { tokenAmount } from '../../utils';
 import { BlockProductionStatsService } from '../../network/service';
+import { ClaimContractService } from '../service';
 
 interface SuppliesResponse {
   koin: number;
@@ -13,6 +14,7 @@ interface SuppliesResponse {
   fdv: number;
   burnedPercentage: number;
   claimed: number;
+  unclaimed: number;
   claimedPercentage: number;
   snapshot: number;
 }
@@ -21,17 +23,52 @@ interface SuppliesResponse {
 export class KoinTotalSupplyController {
   constructor(
     private readonly configService: ConfigService,
+    private readonly claimService: ClaimContractService,
     private readonly totalSupplyService: TotalSupplyService,
     private readonly blockProductionStatsService: BlockProductionStatsService
   ) {}
 
-  @Get('koin/total-supply')
-  async getKoinSupply(useDecimals = true): Promise<number> {
+  private async getKoinSupply(useDecimals = true): Promise<number> {
     return this.totalSupplyService.getTokenSupply(
       this.configService.get<string>('koinos.contracts.koin'),
       8,
       useDecimals
     );
+  }
+
+  @Get('koin/total-supply')
+  async getCirculatingSupply(
+    @Req() req: Request,
+    @Query('useDecimals') useDecimals?: string,
+    @Query('format') format: string = 'text'
+  ): Promise<number | { supply: number }> {
+    const boolUseDecimals = useDecimals !== 'false';
+
+    const claimInfo = await this.claimService.getInfo();
+
+    const snapshot = boolUseDecimals
+      ? tokenAmount(9973874402587865, 8)
+      : 9973874402587865;
+    const claimed = boolUseDecimals
+      ? tokenAmount(Number(claimInfo.koinClaimed), 8)
+      : Number(claimInfo.koinClaimed);
+    const unclaimed = subtract(snapshot, claimed);
+
+    let koinSupply = await this.totalSupplyService.getTokenSupply(
+      this.configService.get<string>('koinos.contracts.koin'),
+      8,
+      boolUseDecimals
+    );
+
+    koinSupply = add(koinSupply, unclaimed);
+
+    const acceptHeader = req.headers['accept'];
+
+    if (acceptHeader && acceptHeader.includes('application/json')) {
+      return { supply: koinSupply };
+    } else {
+      return koinSupply;
+    }
   }
 
   @Get('vhp/total-supply')
@@ -64,27 +101,35 @@ export class KoinTotalSupplyController {
     @Query('useDecimals') useDecimals?: string
   ): Promise<SuppliesResponse> {
     const boolUseDecimals = useDecimals !== 'false';
+    const claimInfo = await this.claimService.getInfo();
 
     const snapshot = boolUseDecimals
-      ? tokenAmount(9973874402587864, 8)
-      : 9973874402587864;
+      ? tokenAmount(9973874402587865, 8)
+      : 9973874402587865;
     const koin = await this.getKoinSupply(boolUseDecimals);
+    const claimed = boolUseDecimals
+      ? tokenAmount(Number(claimInfo.koinClaimed), 8)
+      : Number(claimInfo.koinClaimed);
+    const unclaimed = subtract(snapshot, claimed);
     const vhp = await this.getVhpSupply(boolUseDecimals);
+    const circulating = add(koin, unclaimed);
+    // const virtual = add(circulating, vhp);
     const virtual = add(koin, vhp);
-    const inflation = await this.getInflation(boolUseDecimals);
-    const fdv = add(snapshot, inflation);
+    // const fdv = virtual;
+    const fdv = add(virtual, unclaimed);
+    const inflation = subtract(fdv, snapshot);
     const burned = virtual && vhp ? multiply(divide(vhp, virtual), 100) : 0;
-    const claimed = subtract(virtual, inflation);
     const claimedPercentage = divide(claimed, snapshot) * 100;
 
     return {
-      koin,
+      koin, // : circulating,
       vhp,
       virtual,
       inflation,
       fdv: useDecimals ? fdv : round(fdv, 0),
       burnedPercentage: round(burned, 2),
       claimed,
+      unclaimed,
       claimedPercentage: round(claimedPercentage, 2),
       snapshot,
     };
